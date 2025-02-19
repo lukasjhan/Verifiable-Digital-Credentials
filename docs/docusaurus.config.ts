6ebeee9f07dfd37a7e5355379a3830429a4d2550
Lukas.J.Han <lukas.j.han@gmail.com>
2025-02-19 13:47:57 +0900

    doc: fix baseurl
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,11 +1,11 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Verifiable Digital Credentials (VDCs)',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/digital-credentials',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -80,7 +80,7 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
