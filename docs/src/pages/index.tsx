b92d0665c9c1a3c9ec3f84a7cb5eac030ad624db
Lukas.J.Han <lukas.j.han@gmail.com>
2025-02-23 20:55:47 +0900

    doc: fix main button
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/getStarted/intro"
           >
             Get started - 5min ⏱️
           </Link>
