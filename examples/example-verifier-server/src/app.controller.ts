ec9659ec32d2b0d475e3779383426d87d4972bad
Lukas.J.Han <lukas.j.han@gmail.com>
2025-03-11 23:12:11 +0900

    feat: add sample logic for openid4vp
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- examples/example-verifier-server/src/app.controller.ts
+++ examples/example-verifier-server/src/app.controller.ts
@@ -1,7 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('start')
+  start() {
+    return this.appService.start();
+  }
+
+  @Post('request')
+  request() {
+    return this.appService.request();
+  }
+
+  @Post('response')
+  response(@Body('vp_token') vp_token: Record<string, unknown>) {
+    return this.appService.response(vp_token);
+  }
 }
--- examples/example-verifier-server/src/app.service.ts
+++ examples/example-verifier-server/src/app.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  private readonly verifierUri: string;
+  constructor(private readonly configService: ConfigService) {
+    this.verifierUri = this.configService.get<string>('VERIFIER_URI');
+  }
+
+  start() {
+    const query = new URLSearchParams({
+      client_id: encodeURI(`redirect_uri:${this.verifierUri}`),
+      request_uri: encodeURI(`${this.verifierUri}/request`),
+      request_uri_method: 'post',
+    });
+
+    return `openid4vp://?${query.toString()}`;
+  }
+
+  request() {
+    const result = {
+      client_id: `redirect_uri:${this.verifierUri}`,
+      response_uri: `${this.verifierUri}/response`,
+      response_type: 'vp_token',
+      response_mode: 'direct_post',
+      nonce: 'n-0S6_WzA2Mj',
+      dcql_query: '', // TODO: add query
+    };
+
+    return result;
+  }
+
+  response(vp_token: Record<string, unknown>) {
+    console.log('vp_token', vp_token);
+    return true;
+  }
+}
