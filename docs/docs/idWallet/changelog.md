1f314cf2be5646788c7d91a9961283cc0b7ded26
Lukas.J.Han <lukas@hopae.io>
2025-05-20 22:49:34 +0900

    doc: improve tagline (#111)
    Signed-off-by: Lukas.J Han <lukas@hopae.io>

---
+++ docs/docs/idWallet/changelog.md
@@ -0,0 +1,10 @@
+---
+sidebar_position: 2
+---
+
+# Changelogs
+
+## v0.1.0
+
+- Add initial version
+- Add changelog
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ console.log('BASE_URL: ', process.env.BASE_URL);
 
 const config: Config = {
   title: 'Verifiable Digital Credentials (VDCs)',
-  tagline: 'Secure Digital Identity, Made Simple',
+  tagline: 'The Simplest Wallet SDK',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
